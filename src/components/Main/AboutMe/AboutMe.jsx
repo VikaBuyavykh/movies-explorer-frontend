07c4ddb9ec2e508f67c70527de8a3b1acbb7a322
Vika Buyavykh <vikabuyavykh@gmail.com>
2024-02-11 14:34:05 +0300

    edit about-me-info

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -1,5 +1,5 @@
 import './AboutMe.css';
-import studentPhotoPath from '../../../images/student-img.png';
+import studentPhotoPath from '../../../images/VikaBuyavykh.jpg';
 
 export default function AboutMe() {
     return (
@@ -7,13 +7,16 @@ export default function AboutMe() {
             <h2 className="about-me__title">Студент</h2>
             <div className="about-me__container">
                 <div className="about-me__info">
-                    <h3 className="about-me__name">Виталий</h3>
-                    <p className="about-me__main-info">Фронтенд-разработчик, 30 лет</p>
-                    <p className="about-me__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. 
-                    У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. 
-                    Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». 
-                    После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами 
-                    и ушёл с постоянной работы.</p>
+                    <h3 className="about-me__name">Виктория</h3>
+                    <p className="about-me__main-info">Frontend-разработчик, 28 лет</p>
+                    <p className="about-me__text">Я родилась и выросла в Екатеринбурге, 
+                        с 2016 года живу в Москве, закончила филфак ГосИРЯ им. А.С. Пушкина.
+                        Работала журналистом и пресс-атташе, год назад увлеклась кодингом.
+                        В феврале 2024 успешно окончила факультет веб-разработки Яндекс Практикума, 
+                        получив 293(/300) балла за дипломный проект.
+                        Сейчас продолжаю развиваться в кодинге самостоятельно, но мечтаю продолжить это 
+                        развитие в команде крутых и заряженных ребят. 
+                    </p>
                     <a target="_blank" rel="noopener noreferrer" href="https://github.com/VikaBuyavykh" className="about-me__link">Github</a>
                 </div>
                 <img src={studentPhotoPath} alt="Фотокарточка студента на белом фоне" className="about-me__photo" />
